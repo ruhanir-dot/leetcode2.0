8fc33804d65f2383691f1420bcf8200b216b6384
ruhani rekhi <ruhanirekhi@gmail.com>
2026-02-15 13:52:21 -0800

    2495-NUMBER-OF-UNIQUE-SUBJECTS-TAUGHT-BY-EACH-TEACHER Stats: Time: 267 ms (89%), Space: 68.3 MB (34.77%) - LeetHub

--- 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.py
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.py
@@ -1,11 +1,9 @@
 import pandas as pd
 
 def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
-    teacher_filter = teacher.drop_duplicates(['teacher_id', 'subject_id'])
-    filtered_df = pd.DataFrame( 
-        teacher_filter.groupby('teacher_id')['subject_id'].count()
-    ).reset_index()
-    filtered_df = filtered_df.rename(columns = {
-        'subject_id' :  'cnt'
-        })
-    return filtered_df
+    return teacher.groupby(['teacher_id']).aggregate({
+        'subject_id' : 'nunique'
+    }).reset_index().rename(columns = {
+        'subject_id' : 'cnt'
+    })
+
